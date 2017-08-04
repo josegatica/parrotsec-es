00fd135050f9b7156fd54b140d347f522d1bc415
José Gatica <josegatica@parrotsec.org>
2017-08-04 13:36:41 -0400

    add files

---
+++ footer.php
@@ -0,0 +1,49 @@
+<footer class="mdl-mega-footer">
+            <div class="mdl-mega-footer--middle-section">
+                <div class="mdl-mega-footer--drop-down-section">
+                    <input class="mdl-mega-footer--heading-checkbox" type="checkbox" checked>
+                    <h1 class="mdl-mega-footer--heading">Info</h1>
+                    <ul class="mdl-mega-footer--link-list">
+                        <li><a href="https://docs.parrotsec.org/what-is-parrot">About</a></li>
+                        <li><a href="https://docs.parrotsec.org/terms-and-conditions">Terms</a></li>
+                        <li><a href="https://docs.parrotsec.org/partners">Partners</a></li>
+                        <li><a href="https://blog.parrotsec.org">News</a></li>
+                    </ul>
+                </div>
+                <div class="mdl-mega-footer--drop-down-section">
+                    <input class="mdl-mega-footer--heading-checkbox" type="checkbox" checked>
+                    <h1 class="mdl-mega-footer--heading">Talking about us</h1>
+                    <ul class="mdl-mega-footer--link-list">
+                        <li><a target="_blank" href="https://en.wikipedia.org/wiki/Parrot_Security_OS">Wikipedia</a></li>
+                        <li><a target="_blank" href="http://distrowatch.com/table.php?distribution=parrotsecurity">Distrowatch</a></li>
+                        <li><a target="_blank" href="http://linux.softpedia.com/get/System/Operating-Systems/Linux-Distributions/Parrot-security-OS-102936.shtml">Softpedia</a></li>
+                    </ul>
+                </div>
+                <div class="mdl-mega-footer--drop-down-section">
+                    <input class="mdl-mega-footer--heading-checkbox" type="checkbox" checked>
+                    <h1 class="mdl-mega-footer--heading">Community</h1>
+                    <ul class="mdl-mega-footer--link-list">
+                        <li><a target="_blank" href="https://telegram.me/joinchat/AerigkC4pztMc7Hus7vRZg">Telegram</a></li>
+                        <li><a target="_blank" href="http://lists.parrotsec.org/listinfo">Mailing Lists</a></li>
+                        <li><a target="_blank" href="https://www.facebook.com/groups/parrotsec/">Facebook Group</a></li>
+                    </ul>
+                </div>
+                <div class="mdl-mega-footer--drop-down-section">
+                    <input class="mdl-mega-footer--heading-checkbox" type="checkbox" checked>
+                    <h1 class="mdl-mega-footer--heading">Social Pages</h1>
+                    <ul class="mdl-mega-footer--link-list">
+                        <li><a target="_blank" href="https://dev.parrotsec.org/parrot/">Git</a></li>
+                        <li><a target="_blank" href="https://facebook.com/parrotsec">Facebook</a></li>
+                        <li><a target="_blank" href="https://twitter.com/parrotsec">Twitter</a></li>
+                        <li><a target="_blank" href="https://www.youtube.com/channel/UCj2dezzTc_Oy9eAEwBBodpw">Youtube</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="mdl-mega-footer--bottom-section">
+                <div class="mdl-logo">
+                    Legal Notes
+                </div>
+                Parrot Security OS 2011-<?php echo date(Y,time()) ?> &copy; <a href="https://docs.parrotsec.org/user/palinuro">Lorenzo Faletra</a> - <a href="https://www.gnu.org/licenses/gpl-3.0.en.html" target="_blank">GNU/GPL v3</a><br>
+                <b>Proudly powered in Sicily!</b>
+            </div>
+</footer>
