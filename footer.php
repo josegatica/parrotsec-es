c97ca079925bda253a2b9e3c252e2146461d0696
José Gatica <contacto@josegatica.cl>
2017-08-04 20:54:15 -0400

    Update footer.php
    Traducción íntegra y modificación de algunos contenidos del footer

--- footer.php
+++ footer.php
@@ -4,15 +4,15 @@
                     <input class="mdl-mega-footer--heading-checkbox" type="checkbox" checked>
                     <h1 class="mdl-mega-footer--heading">Info</h1>
                     <ul class="mdl-mega-footer--link-list">
-                        <li><a href="https://docs.parrotsec.org/what-is-parrot">About</a></li>
-                        <li><a href="https://docs.parrotsec.org/terms-and-conditions">Terms</a></li>
+                        <li><a href="https://docs.parrotsec.org/what-is-parrot">Acerca de</a></li>
+                        <li><a href="https://docs.parrotsec.org/terms-and-conditions">Términos</a></li>
                         <li><a href="https://docs.parrotsec.org/partners">Partners</a></li>
-                        <li><a href="https://blog.parrotsec.org">News</a></li>
+                        <li><a href="https://blog.parrotsec.org">Noticias</a></li>
                     </ul>
                 </div>
                 <div class="mdl-mega-footer--drop-down-section">
                     <input class="mdl-mega-footer--heading-checkbox" type="checkbox" checked>
-                    <h1 class="mdl-mega-footer--heading">Talking about us</h1>
+                    <h1 class="mdl-mega-footer--heading">¿Qué se dice de nosotros?</h1>
                     <ul class="mdl-mega-footer--link-list">
                         <li><a target="_blank" href="https://en.wikipedia.org/wiki/Parrot_Security_OS">Wikipedia</a></li>
                         <li><a target="_blank" href="http://distrowatch.com/table.php?distribution=parrotsecurity">Distrowatch</a></li>
@@ -21,11 +21,11 @@
                 </div>
                 <div class="mdl-mega-footer--drop-down-section">
                     <input class="mdl-mega-footer--heading-checkbox" type="checkbox" checked>
-                    <h1 class="mdl-mega-footer--heading">Community</h1>
+                    <h1 class="mdl-mega-footer--heading">Comunidad</h1>
                     <ul class="mdl-mega-footer--link-list">
-                        <li><a target="_blank" href="https://telegram.me/joinchat/AerigkC4pztMc7Hus7vRZg">Telegram</a></li>
-                        <li><a target="_blank" href="http://lists.parrotsec.org/listinfo">Mailing Lists</a></li>
-                        <li><a target="_blank" href="https://www.facebook.com/groups/parrotsec/">Facebook Group</a></li>
+                        <li><a target="_blank" href="https://t.me/ParrotSpanishGroup">Telegram (Grupo en español)</a></li>
+                        <li><a target="_blank" href="http://lists.parrotsec.org/listinfo">Lista de correo</a></li>
+                        <li><a target="_blank" href="https://www.facebook.com/groups/parrotsec/">Grupo global en Facebook (en inglés)</a></li>
                     </ul>
                 </div>
                 <div class="mdl-mega-footer--drop-down-section">
@@ -41,9 +41,9 @@
             </div>
             <div class="mdl-mega-footer--bottom-section">
                 <div class="mdl-logo">
-                    Legal Notes
+                    Notas legales
                 </div>
                 Parrot Security OS 2011-<?php echo date(Y,time()) ?> &copy; <a href="https://docs.parrotsec.org/user/palinuro">Lorenzo Faletra</a> - <a href="https://www.gnu.org/licenses/gpl-3.0.en.html" target="_blank">GNU/GPL v3</a><br>
-                <b>Proudly powered in Sicily!</b>
+                <b>Orgullosamente desarrollado por Lorenzo Faletra y la comunidad de habla hispana de ParrotSec</b>
             </div>
 </footer>
