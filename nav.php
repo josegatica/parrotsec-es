db8c75db19dd6010fec0991af1bd2b4e94427799
José Gatica <contacto@josegatica.cl>
2017-08-05 22:22:52 -0400

    Actualización link Descargas
    Faltaba linkear al archivo de descargas de nuestro dominio.

--- nav.php
+++ nav.php
@@ -8,7 +8,7 @@
         </div>
         <div class="mdl-layout__tab-bar mdl-js-ripple-effect mdl-color--primary-dark">
             <a href="https://www.parrotsec-es.org/" <?php if($page=="home"){?>class="mdl-layout__tab is-active"<?php } else { ?>class="mdl-layout__tab"<?php } ?>>Inicio</a>
-            <a href="https://www.parrotsec.org/download.fx" <?php if($page=="download"){?>class="mdl-layout__tab is-active"<?php } else { ?>class="mdl-layout__tab"<?php } ?>>Descargas</a>
+            <a href="https://www.parrotsec-es.org/download.fx" <?php if($page=="download"){?>class="mdl-layout__tab is-active"<?php } else { ?>class="mdl-layout__tab"<?php } ?>>Descargas</a>
             <a href="https://blog.parrotsec.org/" class="mdl-layout__tab">Noticias</a>
             <a href="https://docs.parrotsec.org/" class="mdl-layout__tab">Documentación</a>
             <a href="https://community.parrotsec.org/viewforum.php?id=25" class="mdl-layout__tab">Comunidad</a>
