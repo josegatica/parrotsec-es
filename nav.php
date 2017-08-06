2f3a831eede5f10f6f8ec7c3df887be8cf09a264
José Gatica <contacto@josegatica.cl>
2017-08-06 16:47:07 -0400

    Update nav.php
    Redirección de algunos elementos del menú principal

--- nav.php
+++ nav.php
@@ -10,8 +10,8 @@
             <a href="https://www.parrotsec-es.org/" <?php if($page=="home"){?>class="mdl-layout__tab is-active"<?php } else { ?>class="mdl-layout__tab"<?php } ?>>Inicio</a>
             <a href="https://www.parrotsec-es.org/download.php" <?php if($page=="download"){?>class="mdl-layout__tab is-active"<?php } else { ?>class="mdl-layout__tab"<?php } ?>>Descargas</a>
             <a href="https://blog.parrotsec.org/" class="mdl-layout__tab">Noticias</a>
-            <a href="https://docs.parrotsec.org/" class="mdl-layout__tab">Documentación</a>
-            <a href="https://community.parrotsec.org/viewforum.php?id=25" class="mdl-layout__tab">Comunidad</a>
-            <a href="https://docs.parrotsec.org/FAQ" class="mdl-layout__tab">FAQ</a>
+            <a href="https://docs.parrotsec.org/" target="blank" class="mdl-layout__tab">Documentación</a>
+            <a href="https://community.parrotsec.org/viewforum.php?id=25" target="blank" class="mdl-layout__tab">Comunidad</a>
+            <a href="https://docs.parrotsec.org/FAQ" target="blank" class="mdl-layout__tab">FAQ</a>
         </div>
     </header>
