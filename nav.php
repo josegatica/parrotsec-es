6752045913bb7aa2399346b221b45f1d1115cc2d
Claudio Marcial Peon <claudio.marcial@datacor.com.ar>
2017-08-21 00:27:14 +0100

    Update FAQ to Wiki

--- nav.php
+++ nav.php
@@ -12,6 +12,6 @@
             <a href="https://blog.parrotsec.org/" class="mdl-layout__tab">Noticias</a>
             <a href="https://docs.parrotsec-es.org/" target="blank" class="mdl-layout__tab">Documentación</a>
             <a href="https://community.parrotsec.org/viewforum.php?id=25" target="blank" class="mdl-layout__tab">Comunidad</a>
-            <a href="https://github.com/josegatica/parrot-docu-es/blob/master/00.-%20FAQ.md" target="blank" class="mdl-layout__tab">FAQ</a>
+            <a href="https://docs.parrotsec-es.org/doku.php?id=faq" target="blank" class="mdl-layout__tab">FAQ</a>
         </div>
     </header>
