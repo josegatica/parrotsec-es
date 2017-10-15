25cae8b9b177de902e740173c8318035675ea457
José Gatica <josegatica@parrotsec.org>
2017-10-15 00:09:32 -0300

    Update videos.php

--- videos.php
+++ videos.php
@@ -68,29 +68,41 @@
 </header>
 	<body style="padding-top: 150px;" class="mdl-parrot mdl-color--grey-100 mdl-color-text--grey-700 mdl-base">
 		<center>
+			<hr>
 			<br>
 			<iframe width="560" height="315" src="https://www.youtube.com/embed/6AkhIOCvmYQ?rel=0" frameborder="0" allowfullscreen></iframe>
 			<br>
+			<hr>
 			<iframe width="560" height="315" src="https://www.youtube.com/embed/9_BZw2n7MSU?rel=0" frameborder="0" allowfullscreen></iframe>
 			<br>
+			<hr>
 			<iframe width="560" height="315" src="https://www.youtube.com/embed/TBGUf3oSjvI?rel=0" frameborder="0" allowfullscreen></iframe>
 			<br>
+			<hr>
 			<iframe width="560" height="315" src="https://www.youtube.com/embed/3LD6-6It_5Q?rel=0" frameborder="0" allowfullscreen></iframe>
 			<br>
+			<hr>
 			<iframe width="560" height="315" src="https://www.youtube.com/embed/f9SdFAJuyPo?rel=0" frameborder="0" allowfullscreen></iframe>
 			<br>
+			<hr>
 			<iframe width="560" height="315" src="https://www.youtube.com/embed/WwIiI398kmE?rel=0" frameborder="0" allowfullscreen></iframe>
 			<br>
+			<hr>
 			<iframe width="560" height="315" src="https://www.youtube.com/embed/afuPepqpCpk?rel=0" frameborder="0" allowfullscreen></iframe>
 			<br>
+			<hr>
 			<iframe width="560" height="315" src="https://www.youtube.com/embed/1HcGUP90eMU?rel=0" frameborder="0" allowfullscreen></iframe>
 			<br>
+			<hr>
 			<iframe width="560" height="315" src="https://www.youtube.com/embed/JvSDU5ZqVs8?rel=0" frameborder="0" allowfullscreen></iframe>
 			<br>
+			<hr>
 			<iframe width="560" height="315" src="https://www.youtube.com/embed/F6X_4jJn3OA?rel=0" frameborder="0" allowfullscreen></iframe>
 			<br>
+			<hr>
 			<iframe width="560" height="315" src="https://www.youtube.com/embed/1527EPck4NY?rel=0" frameborder="0" allowfullscreen></iframe>
 			<br>
+			<hr>
 			<iframe width="560" height="315" src="https://www.youtube.com/embed/t7Nx3q00XjQ?rel=0" frameborder="0" allowfullscreen></iframe>
 			<br>
 		</center>
