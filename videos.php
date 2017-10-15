890ec980f8a564f7fc3e3f602ad273c5f102e22b
José Gatica <josegatica@parrotsec.org>
2017-10-14 22:41:01 -0300

    Added videotutorials in spanish

--- videos.php
+++ videos.php
@@ -6,6 +6,117 @@
 		?>
 	</head>
 	<body class="mdl-parrot mdl-color--grey-100 mdl-color-text--grey-700 mdl-base">
-		<h1>Página en Construcción</h1>
+		<center>
+			<br>
+			<iframe width="560" height="315" src="https://www.youtube.com/embed/6AkhIOCvmYQ?rel=0" frameborder="0" allowfullscreen></iframe>
+			<br>
+			<iframe width="560" height="315" src="https://www.youtube.com/embed/9_BZw2n7MSU?rel=0" frameborder="0" allowfullscreen></iframe>
+			<br>
+			<iframe width="560" height="315" src="https://www.youtube.com/embed/TBGUf3oSjvI?rel=0" frameborder="0" allowfullscreen></iframe>
+			<br>
+			<iframe width="560" height="315" src="https://www.youtube.com/embed/3LD6-6It_5Q?rel=0" frameborder="0" allowfullscreen></iframe>
+			<br>
+			<iframe width="560" height="315" src="https://www.youtube.com/embed/f9SdFAJuyPo?rel=0" frameborder="0" allowfullscreen></iframe>
+			<br>
+			<iframe width="560" height="315" src="https://www.youtube.com/embed/WwIiI398kmE?rel=0" frameborder="0" allowfullscreen></iframe>
+			<br>
+			<iframe width="560" height="315" src="https://www.youtube.com/embed/afuPepqpCpk?rel=0" frameborder="0" allowfullscreen></iframe>
+			<br>
+			<iframe width="560" height="315" src="https://www.youtube.com/embed/1HcGUP90eMU?rel=0" frameborder="0" allowfullscreen></iframe>
+			<br>
+			<iframe width="560" height="315" src="https://www.youtube.com/embed/JvSDU5ZqVs8?rel=0" frameborder="0" allowfullscreen></iframe>
+			<br>
+			<iframe width="560" height="315" src="https://www.youtube.com/embed/F6X_4jJn3OA?rel=0" frameborder="0" allowfullscreen></iframe>
+			<br>
+			<iframe width="560" height="315" src="https://www.youtube.com/embed/1527EPck4NY?rel=0" frameborder="0" allowfullscreen></iframe>
+			<br>
+			<iframe width="560" height="315" src="https://www.youtube.com/embed/t7Nx3q00XjQ?rel=0" frameborder="0" allowfullscreen></iframe>
+			<br>
+		</center>
+	<footer>
+    <div class="container-fluid">
+      <div class="row footer-links-area">
+        <div class="col-sm-5">
+          <div class="row">
+            <div class="col-xs-4">
+              <ul>
+                <li>
+                  <a href="https://docs.parrotsec.org/what-is-parrot">
+                    <span>About</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://docs.parrotsec.org/terms-and-conditions">
+                    <span>Terms</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://docs.parrotsec.org/partners">
+                    <span>Partners</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div class="col-xs-4">
+              <ul>
+                <li>
+                  <a href="https://en.wikipedia.org/wiki/Parrot_Security_OS">
+                    <span>Wikipedia</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://distrowatch.com/table.php?distribution=parrotsecurity">
+                    <span>Distrowatch</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div class="col-xs-4">
+              <ul>
+                <li>
+                  <a href="http://chat.frozenbox.org:3989/?nick=parrot_....&channels=parrot&prompt=1">
+                    <span>IRC</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://telegram.me/joinchat/AerigkC4pztMc7Hus7vRZg">
+                    <span>Telegram</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="http://lists.parrotsec.org/listinfo">
+                    <span>Mailing lists</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://www.facebook.com/groups/parrotsec/">
+                    <span>Facebook Group</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-5 col-sm-offset-2 social-networks">
+          <a href="https://www.facebook.com/parrot.es/" target="_blank" class="wow fadeIn" data-wow-delay=".6s">
+            <i class="fa fa-facebook-square"></i>
+          </a>
+          <a href="https://twitter.com/ParrotSec_es" target="_blank" class="wow fadeIn">
+            <i class="fa fa-twitter-square"></i>
+          </a>
+          <a href="https://github.com/josegatica/parrotsec-es" target="_blank" class="wow fadeIn" data-wow-delay=".3s">
+            <i class="fa fa-github-square"></i>
+          </a>
+          <a href="https://www.youtube.com/channel/UCj2dezzTc_Oy9eAEwBBodpw" target="_blank" class="wow fadeIn" data-wow-delay=".9s">
+            <i class="fa fa-youtube-square"></i>
+          </a>
+        </div>
+      </div>
+      <div class="blue-triangular-division wow slideInDown"></div>
+      <p class="text-center">© Lorenzo Faletra & ParrotSec Spanish Community Algunos derechos reservados.</span> - <a href="https://www.gnu.org/licenses/gpl-3.0.en.html">GNU/GPL v3</a></p>
+      <p class="text-center">¡Orgullosamente desarrollado por la comunidad de habla hispana de Parrot Security OS!</p>
+      </p>
+    </div>
+</footer>
 	</body>
 </html>
